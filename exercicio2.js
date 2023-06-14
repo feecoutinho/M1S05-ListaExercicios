eb0bfdf9bd7d18e58d0a484fad2a451c1b560610
Fernanda Coutinho <fernandarcoutinho@gmail.com>
2023-06-13 22:13:00 -0300

    ex2 resolvido

--- exercicio2.js
+++ exercicio2.js
@@ -5,3 +5,35 @@
  * a tradução para a mesma cor em português. Utiliza uma estrutura condicional (switch)
  * para fazer o mapeamento das cores.
  */
+
+var cor_ingles = 'red'
+
+ switch (cor_ingles.toLowerCase()) {
+    case 'blue':
+        console.log('Azul');
+        break;
+    
+    case 'red':
+        console.log('Vermelho');
+        break;
+        
+    case 'pink':
+        console.log('Rosa');
+        break;
+
+    case 'orange':
+        console.log('Laranja');
+        break; 
+
+    case 'white':
+        console.log('Branco');
+        break;
+    
+
+    case 'green':
+        console.log('Verde');
+        break;   
+
+    default: 
+    console.log('Inválido')
+}
