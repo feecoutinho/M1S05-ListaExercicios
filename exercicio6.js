72e4fb0bca82b7fa7eef1c83c048f123f1b5fb2f
Fernanda Coutinho <fernandarcoutinho@gmail.com>
2023-06-16 22:38:07 -0300

    EX 6 resolvido

--- exercicio6.js
+++ exercicio6.js
@@ -5,3 +5,35 @@
  * e o preço de cada produto. Após imprimir essas informações, imprime o total de produtos
  * e o valor total de todos os produtos.
  */
+
+let produto1 = {
+    nome: 'felix felicis',
+    preço: 25
+} 
+
+let produto2 = {
+    nome: 'veritasserum',
+    preço: 15
+} 
+
+let produto3 = {
+    nome: 'amortentia',
+    preço: 5
+} 
+
+let produto4 = {
+    nome: 'polissuco',
+    preço: 15
+} 
+
+let produtos = []
+    produtos.push(produto1, produto2, produto3, produto4)
+
+let somatotal = 0
+
+for(const produto of produtos) {
+    console.log(produto)
+    somatotal = somatotal + produto.preço;
+}
+
+console.log(`O valor total dos produtos é de ${somatotal} galeões`)
